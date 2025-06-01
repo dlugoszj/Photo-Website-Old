a088fbc6b0c14080032f498a98c2a647e34f5e96
dlugoszj <dlugoszj@umich.edu>
2025-06-01 19:22:56 -0400

    t

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Photo-Website-Old',
+  base: '/Photo-Website-Old/',
   plugins: [tailwindcss(),
     react()
   ],
